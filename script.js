2465b3fe80585944fbf7f8598f494ac97465f8d2
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-13 19:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let fancyCalculator = (first, second) => {
+  // Write your code here
+
+  return first * 2 + second;
+};
+
+// Do not change the code below
+
+const first = prompt("Enter the first number - ");
+const second = prompt("Enter the second number - ");
+alert(
+  "The final result is : " + fancyCalculator(parseInt(first), parseInt(second))
+);
